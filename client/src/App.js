b689377ae557f633d026f42d084f0ec938449839
miko <ptechasa@mail.ccsf.edu>
2019-11-16 13:47:48 -0800

    intial files

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Container, Row, Col} from 'react-bootstrap';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
